0d79c263f24570307dc32fa23ca296f4a7168a1b
SiwyKot41 <frutkowski80@gmail.com>
2024-03-16 13:02:54 +0100

    feat: add third card of product presentation

--- components/ui/BlankTask.vue
+++ components/ui/BlankTask.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
-defineProps<{
-  drag?: boolean
+const props = defineProps<{
+  border?: boolean
+  borderColor?: string
+  done?: boolean
 }>()
+
+const borderColor = toRef(props, 'borderColor')
+const { borderColor: borderColorStyle } = useBorderColor(borderColor)
+
 </script>
 <template>
   <div
     class="bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex items-center"
-    :class="drag ? 'border-2 border-indigo-800' : ''"
+    :class="border ? `border-2 ${borderColorStyle}` : ''"
   >
-    <div class="shrink-0 w-[0.30rem] h-[0.30rem] bg-slate-400" />
+    <div v-if="!done">
+      <div class="shrink-0 w-[0.30rem] h-[0.30rem] bg-slate-400" />
+    </div>
+    <div v-else>
+      <TaskDoneMark />
+    </div>
     <div class="ml-2 w-28 h-4 bg-gray-200 rounded-lg" />
   </div>
 </template>
+
+<style scoped>
+  </style>
---
+++ components/ui/TaskDoneMark.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  size?: string
+}>(), {
+  size: 'sm'
+})
+
+const sideLength = ref()
+const markWidth = ref()
+const markHeight = ref()
+const markTop = ref()
+const markLeft = ref()
+switch (props.size) {
+  case 'md':
+    markTop.value = '3.3px'
+    markLeft.value = '5.5px'
+    markWidth.value = '5px'
+    markHeight.value = '8px'
+    sideLength.value = '1.3em'
+    break
+  case 'sm':
+  default:
+    markTop.value = '2px'
+    markLeft.value = '4px'
+    markWidth.value = '4px'
+    markHeight.value = '7px'
+    sideLength.value = '1em'
+    break
+}
+</script>
+<template>
+  <input
+    type="checkbox"
+    disabled
+    checked
+  >
+</template>
+<style scoped>
+input[type="checkbox"] {
+  position: relative;
+  width: v-bind(sideLength);
+  height: v-bind(sideLength);
+  border-radius: 4px;
+  appearance: none;
+  &::before {
+    position: absolute;
+    content: '';
+    display: block;
+    top: v-bind(markTop);
+    left:v-bind(markLeft);
+    width: v-bind(markWidth);
+    height: v-bind(markHeight);
+    border-style: solid;
+    border-color: white;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+    opacity: 0;
+  }
+  &:checked {
+    border-color: green;
+    @apply bg-green-600;
+    &::before {
+      opacity: 1;
+    }
+  }
+}
+</style>
