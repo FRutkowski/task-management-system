fbfa0f2d2dcdd35cc38303e63a58654f38fa4e44
SiwyKot41 <frutkowski80@gmail.com>
2024-09-07 11:12:18 +0200

    add target option in nuxt.config.ts to static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt', 'nuxt-icon', '@nuxt/image', '@nuxtjs/tailwindcss'],
+  target: 'static',
   ui: {
     global: true
   },
